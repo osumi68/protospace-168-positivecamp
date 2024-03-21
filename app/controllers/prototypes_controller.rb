6c3447a2d9756103f91091d6744a175cf4fb9d3b
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-21 18:46:11 +0900

    createアクションの内容を設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -13,13 +13,20 @@ end
 
 def create
 
+  @prototype = Prototype.create(prototype_params)
+  if @prototype.save
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entitiy
+  end
+
 end
 
 
 private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :image, :catch_copy, :concept )
+    params.require(:prototype).permit(:title, :image, :catch_copy, :concept ).merge(user_id: current_user.id)
   end
 
 
