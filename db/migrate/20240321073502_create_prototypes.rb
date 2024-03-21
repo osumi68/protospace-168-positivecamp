e7e420ff46903e7d728f0bce9f003c4c76554059
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-21 16:43:21 +0900

    リファレンス型の記述編集

--- db/migrate/20240321073502_create_prototypes.rb
+++ db/migrate/20240321073502_create_prototypes.rb
@@ -1,24 +1,13 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      t.string  :title,      null: false
-      t.text    :catch_copy, null: false
-      t.ctext   :concept,    null: false
-      t.user    :references, null: false, foreign_key: true
+      t.string        :title,      null: false
+      t.text          :catch_copy, null: false
+      t.text          :concept,    null: false
+      t.references    :user,       null: false, foreign_key: true
       t.timestamps
     end
   end
 end
 
 
-
-
-| Column     | Type       | Options     |
-| ---------- | ---------- | ----------- |
-| title      | string     | null: false |
-| catch_copy | text       | null: false |
-| concept    | text       | null: false |
-| user       | references | null: false, foreign_key: true |
-### Association
-- belongs_to :user
-- has_many :comments
