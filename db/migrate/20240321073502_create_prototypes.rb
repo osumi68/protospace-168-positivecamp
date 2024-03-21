45e1b2953afbaa4ec77f4134b423240aeeb3f320
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-21 16:39:33 +0900

    prototypeモデルのカラムと型を記述

--- db/migrate/20240321073502_create_prototypes.rb
+++ db/migrate/20240321073502_create_prototypes.rb
@@ -1,8 +1,24 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-
+      t.string  :title,      null: false
+      t.text    :catch_copy, null: false
+      t.ctext   :concept,    null: false
+      t.user    :references, null: false, foreign_key: true
       t.timestamps
     end
   end
 end
+
+
+
+
+| Column     | Type       | Options     |
+| ---------- | ---------- | ----------- |
+| title      | string     | null: false |
+| catch_copy | text       | null: false |
+| concept    | text       | null: false |
+| user       | references | null: false, foreign_key: true |
+### Association
+- belongs_to :user
+- has_many :comments
