a1d2cb28acf416ff60aaa7dda5b57f16b547d008
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-22 15:39:49 +0900

    ログインユーザ＝投稿者の場合の『編集』『削除』表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_22_030243) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_21_084851) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,8 +43,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_22_030243) do
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_22_030243) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "prototypes", "users"
 end
